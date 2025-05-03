bb74fd4ca015fd49ee669bda83d62ee363a06787
Your Name <you@example.com>
2025-05-03 16:01:42 -0400

    axios req for backend for lastUpdated

--- backend/api/lastUpdated.js
+++ backend/api/lastUpdated.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
