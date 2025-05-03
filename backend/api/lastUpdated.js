bde75ff45e32ac2450660e37c42da0bceb5aae31
Your Name <you@example.com>
2025-05-03 15:51:24 -0400

    fix to CORS for lastUpdated api route

--- backend/api/lastUpdated.js
+++ backend/api/lastUpdated.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://ttv.danishmohammed.ca");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
